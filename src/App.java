99d0a1f5eb56df2e798dfb1d73394b2037b624a2
bernal castillo santiago <bernalcastillosantiago@gmail.com>
2026-08-18 19:50:30 -0500

    SubiendoClase

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        int a = 10, b = 3 ;
+        System.out.println( a + b );
+        System.out.println( a - b );
+        System.out.println( a * b );
+        System.out.println( a / b );
+        System.out.println( a % b );
+        System.out.println( 10.0 / 3);
+        
     }
 }
---
+++ src/MiClase.java
@@ -0,0 +1,9 @@
+public class MiClase {
+    private int edad ;
+    private double altura ;
+    private char sexo = 'm' ;
+    private boolean aprobado = false ;
+    private String nombre = " Santiago Bernal Castillo " ;
+    private String saludo = "Hola, " + nombre ;
+    private double nota = 4.5 ;
+}
